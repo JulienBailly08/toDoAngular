b40a7dc0e0a5a3d3ddd97ba50d56449133d91e71
Julien_B <julienbailly08@gmail.com>
2021-09-27 22:54:57 +0200

    bootstrap and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
