b198c77030b7a2450fab1a3ee086874ead20a443
Julien_B <julienbailly08@gmail.com>
2021-10-03 09:15:05 +0200

    event binding

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoComponent implements OnInit {
 
+  toDoOne:string="Poulpe";
+  toDos = [
+    {
+      todoName: 'projet 1',
+      todoStatus: true,
+      image:"http://placehold.it/150"
+    },
+    {
+      todoName: 'projet 2',
+      todoStatus: false,
+      image:"http://placehold.it/150"
+    },
+    {
+      todoName: 'projet 3',
+      todoStatus: false,
+      image:"http://placehold.it/150"
+    },
+    {
+      todoName: 'projet 4',
+      todoStatus: true,
+      image:"http://placehold.it/150"
+    },
+  ];
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChangeStatus(i:number){
+    this.toDos[i].todoStatus = !this.toDos[i].todoStatus;
   }
 
 }
