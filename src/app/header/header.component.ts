ae07ae67d56ac0f2c875726a34e2f43b5a3d0e45
Julien_B <julienbailly08@gmail.com>
2021-10-05 16:26:11 +0200

    simplification de la fonction de chrono via un observable deja existant

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { Observable, Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-header',
@@ -8,67 +8,28 @@ import { Observable, Subscription } from 'rxjs';
 })
 export class HeaderComponent implements OnInit, OnDestroy {
 
-  salutationSub:any;
-  nombresPairsSub:any;
+
+  secondeSub:any;
   valeurVersAffichage:any;
-  helloWorldVersTemplate:any;
+
 
   constructor() { }
 
   ngOnInit(): void {
-    const saluation = new Observable((valeurRetour) => {
-      setTimeout(() => {
-        valeurRetour.next("hello");
-      }, 500);
-      setTimeout(() => {
-        valeurRetour.next("world !");
-      }, 2000);
-      setTimeout(() => {
-        valeurRetour.next("etc, etc...");
-      }, 3500);
-      setTimeout(() => {
-        valeurRetour.next(":d");
-      }, 5000);
-      setTimeout(() => {
-        valeurRetour.complete();
-      }, 6000);
-    });
 
-    const nombresPairs = new Observable((observer) => {
-      let value = 0;
-      const interval = setInterval(() => {
-        if (value % 2 == 0) {
-          observer.next(value);
-        }
-        value++;
-      }, 500);
-      return () => clearInterval(interval);
-    });
+    const secondeObs = interval(1000);
 
-    this.nombresPairsSub = nombresPairs.subscribe(
+    this.secondeSub = secondeObs.subscribe(
       (value) => {
-        console.log("nombre pair :" + value);
         this.valeurVersAffichage =value;
       }
     );
 
-    this.salutationSub = saluation.subscribe(
-      (value) => {
-        console.log("value :" + value);
-        this.helloWorldVersTemplate = value;
-      },
-      (error) => {
-        console.log("erreur :" + error);
-      },
-      () => {
-        console.log("observable complété");
-      },
-    );
 
   }
   ngOnDestroy(){
-    this.salutationSub.unsubsribe();
-    this.nombresPairsSub.unsubsribe();
+
+    this.secondeSub.unsubsribe();
   }
 }
 
