8e705f1c55469ff2a2b83cd826e0dbed82fb0e95
Julien_B <julienbailly08@gmail.com>
2021-10-11 15:34:53 +0200

    recuperation données vers services et affichage des users

--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -6,7 +6,20 @@ import { User } from '../models/user.model';
 })
 export class UsersService {
 
-  users:User[]=[];
+  users:User[]=[{
+      firstname:'julien',
+      lastname:'bailly',
+      email:'breubit@laposte.net',
+      address:{
+        street:'2 montée du Mas du Castellet',
+        city:'Beauvallon',
+        state:'Drôme',
+        zip:26800
+      },
+      description:'Lorem Ipsum Dolores sit Amer etc etc etc...',
+      dateBirth:'08-03-1979'
+  }];
+
   constructor() { }
 
   addUser(user:User):void{
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../models/user.model';
+import { UsersService } from '../services/users.service';
 
 @Component({
   selector: 'app-users',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService:UsersService) { }
+
+  users:User[]=[];
 
   ngOnInit(): void {
+    this.users = this.userService.users;
   }
 
 }
